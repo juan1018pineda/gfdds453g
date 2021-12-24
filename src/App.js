6a9bab5dace82b0b480d0c8ca5c1ef2122288e4f
pineda.juan92 <pineda.juan92@gmail.com>
2021-12-23 19:38:46 -0500

    Nav, Switch, Routes and Router implemented

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
-import React, { Component } from 'react';
-import {
-  Switch,
-  Route,
-  Redirect,
-  Link
-} from 'react-router-dom';
+import React, { Component } from "react";
+import { Switch, Route, Redirect, Link, BrowserRouter } from "react-router-dom";
 
 export class App extends Component {
   render() {
     return (
       <div>
-        <nav>
-          {/*Aca deben ir los links de navegacion*/}
-        </nav>
-        {/* Aca tienes que agreager algo para que las rutas funcionen*/}
+          <nav>
+            <Link to="/page1">Page 1</Link>
+            <Link to="/page2">Page 2</Link>
+          </nav>
+          <Switch>
+            <Redirect exact from="/" to="/page1"></Redirect>
+            <Route path="/page1" component={Page1}></Route>
+            <Route path="/page2" component={Page2}></Route>
+            <Route path="/" component={NotFound}></Route>
+          </Switch>
       </div>
-    )
+    );
   }
 }
 
 // No tienes que hacer nada por debajo de esta linea
 //==================================================
 
-
 export const Page1 = (props) => {
-  return (
-    <h1>Pagina 1</h1>
-  )
-}
+  return <h1>Pagina 1</h1>;
+};
 
 export const Page2 = (props) => {
-  return (
-    <h1>Pagina 2</h1>
-  )
-}
+  return <h1>Pagina 2</h1>;
+};
 
 export const NotFound = (props) => {
-  return (
-    <h1>Pagina no encontrada</h1>
-  )
-}
+  return <h1>Pagina no encontrada</h1>;
+};
